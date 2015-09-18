5a74fe28c726fcd300f41a49d5f720e261cbd2f4
Tony Bowden <tony@tmtm.com>
2015-09-18 21:27:04 +0100

    Rodrigues has two seats (all others have three)

--- scraper.rb
+++ scraper.rb
@@ -22,24 +22,28 @@ def scrape_list(url)
   noko = noko_for(url)
   noko.css('.circonscrption h1').each do |h1|
     matched = h1.text.match(/No. (\d+) - (.*)/) or raise "Odd area: #{h1.text}" 
+    area_id, area = matched.captures
 
-    winner = h1.xpath('./following-sibling::table//tr[td]').first 
-    tds = winner.css('td')
-    sort_name = tds[0].text.tidy.sub(/^\d+\. /,'')
-    family_name, given_name = sort_name.split(', ', 2)
+    winners = (area_id.to_i == 21) ? 2 : 3
+    h1.xpath('./following-sibling::table//tr[td]').take(winners).each do |winner|
+      tds = winner.css('td')
+      sort_name = tds[0].text.tidy.sub(/^\d+\. /,'')
+      family_name, given_name = sort_name.split(', ', 2)
 
-    data = { 
-      name: "#{given_name} #{family_name}",
-      sort_name: sort_name,
-      given_name: given_name,
-      sort_name: sort_name,
-      party: tds[2].text.tidy,
-      area_id: matched.captures.first,
-      area: matched.captures.last,
-      term: 2014,
-      source: url,
-    }
-    ScraperWiki.save_sqlite([:name, :area, :term], data)
+      data = { 
+        name: "#{given_name} #{family_name}",
+        sort_name: sort_name,
+        given_name: given_name,
+        sort_name: sort_name,
+        party: tds[2].text.tidy,
+        area_id: area_id,
+        area: area,
+        term: 2014,
+        source: url,
+      }
+      # puts data
+      ScraperWiki.save_sqlite([:name, :area, :term], data)
+    end
   end
 end
 
